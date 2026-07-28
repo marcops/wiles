8a4c3a2a7f1762f99d642d205d746f11f71a6985
Marco Stefani <marco@MacBook-Air-de-Paula.local>
2026-07-28 17:27:17 -0300

    Include release asset v0.0.1 and update Cask URL

--- Casks/wiles.rb
+++ Casks/wiles.rb
@@ -2,7 +2,7 @@ cask "wiles" do
   version "0.0.1"
   sha256 "28092067b987591c425d6a4fcc1dd58e3483212efe8a908531914d7bbc5171e3"
 
-  url "https://github.com/marcops/wiles/releases/download/v#{version}/wiles-v#{version}.zip"
+  url "https://raw.githubusercontent.com/marcops/wiles/main/releases/wiles-v#{version}.zip"
   name "Wiles"
   desc "Fast, native macOS file manager"
   homepage "https://github.com/marcops/wiles"
