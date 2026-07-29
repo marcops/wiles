a35cadbb5937d8252dcb8b3c1e0aa67128d34bc1
Marco Stefani <marco@Mac.vodafone.ultrahub>
2026-07-29 17:26:37 -0300

    Bust GitHub CDN cache by using exact commit SHA in url

--- Casks/wiles.rb
+++ Casks/wiles.rb
@@ -2,7 +2,7 @@ cask "wiles" do
   version "0.0.4"
   sha256 "922591f2ba7e4d3eca9a266e634b260410888bd42858644a93bc867da9638fe9"
 
-  url "https://raw.githubusercontent.com/marcops/wiles/main/releases/wiles-v#{version}.zip"
+  url "https://raw.githubusercontent.com/marcops/wiles/d36aa172333c2f5e1ab7e37f02cc1bc58df5c6a8/releases/wiles-v#{version}.zip"
   name "Wiles"
   desc "Ultra-fast modern macOS File Manager"
   homepage "https://github.com/marcops/wiles"
