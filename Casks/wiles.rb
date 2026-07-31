c0f7108cb1b7805012d3058ad36e0306b8613f98
Marco Stefani <marco@Mac.vodafone.ultrahub>
2026-07-31 12:18:23 -0300

    cask: update v0.0.8 formula with compiled 0.0.8 SHA256 and commit URL

--- Casks/wiles.rb
+++ Casks/wiles.rb
@@ -1,8 +1,8 @@
 cask "wiles" do
   version "0.0.8"
-  sha256 "6ca3fdfe30055e97275a3f02797bcd8272ada97fb46762bd9f91a99d9c2b5b24"
+  sha256 "e34337411f7a299b0a08d42a9c7776eff157584d0180d0a9b5bfa43e5587d5df"
 
-  url "https://raw.githubusercontent.com/marcops/wiles/725b05eb2c6f0bb93014c6ef5fec79e9885becf5/releases/wiles-v#{version}.zip"
+  url "https://raw.githubusercontent.com/marcops/wiles/60f72ba4d3d83dd2c063efdcb98835c07396403a/releases/wiles-v#{version}.zip"
   name "Wiles"
   desc "Ultra-fast modern macOS File Manager"
   homepage "https://github.com/marcops/wiles"
