b164f55426ea6311bb0076092869a2fc9de56516
Marco Stefani <marco@Mac.vodafone.ultrahub>
2026-07-31 11:01:46 -0300

    cask: bump wiles to v0.0.8 with CDN-safe commit SHA URL

--- Casks/wiles.rb
+++ Casks/wiles.rb
@@ -1,8 +1,8 @@
 cask "wiles" do
-  version "0.0.7"
-  sha256 "08fc97949a88e44422c4c96d5f05c01dc1cb7be4f752494a6df0966b33d9aac9"
+  version "0.0.8"
+  sha256 "6ca3fdfe30055e97275a3f02797bcd8272ada97fb46762bd9f91a99d9c2b5b24"
 
-  url "https://raw.githubusercontent.com/marcops/wiles/08d37b6ab205b23aa64266ea73a7e6beaa529914/releases/wiles-v#{version}.zip"
+  url "https://raw.githubusercontent.com/marcops/wiles/725b05eb2c6f0bb93014c6ef5fec79e9885becf5/releases/wiles-v#{version}.zip"
   name "Wiles"
   desc "Ultra-fast modern macOS File Manager"
   homepage "https://github.com/marcops/wiles"
