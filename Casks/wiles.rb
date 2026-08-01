52fa9aa556bef8da81af4f25ee2796997a651926
Marco Stefani <marco@MacBook-Air-de-Paula.local>
2026-07-31 21:13:32 -0300

    release: update Homebrew cask to v0.1.0

--- Casks/wiles.rb
+++ Casks/wiles.rb
@@ -1,8 +1,8 @@
 cask "wiles" do
-  version "0.0.10"
-  sha256 "f8a7dcd1c57f0590c347fb2f5f44ee3a452371c877a3844dc42ef4ada19ff240"
+  version "0.1.0"
+  sha256 "a7df222c7779b2a97affc6a28da0ad844dfadace4d4f09c8e3602265f6e46594"
 
-  url "https://raw.githubusercontent.com/marcops/wiles/29b0c3e50237893a93f65985af2838bbc078058f/releases/wiles-v#{version}.zip"
+  url "https://raw.githubusercontent.com/marcops/wiles/2b547c7312ec2921b6b2bc918a128dacbd375020/releases/wiles-v#{version}.zip"
   name "Wiles"
   desc "Ultra-fast modern macOS File Manager"
   homepage "https://github.com/marcops/wiles"
