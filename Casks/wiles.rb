4ac134f97fcab2b09c1df91b3aaa109a843436cb
Marco Stefani <marco@MacBook-Air-de-Paula.local>
2026-08-03 05:22:24 -0300

    release: update Homebrew Cask URL to v0.1.2 commit SHA

--- Casks/wiles.rb
+++ Casks/wiles.rb
@@ -2,7 +2,7 @@ cask "wiles" do
   version "0.1.2"
   sha256 "3c44aff6eec80be395eefcf5fcea1489353ebf5cc9493d53417c07361b99df43"
 
-  url "https://raw.githubusercontent.com/marcops/wiles/6350ea5b22224c0a1b0b3c1e4339ac726f8ba385/releases/wiles-v#{version}.zip"
+  url "https://raw.githubusercontent.com/marcops/wiles/8007ac28a89b130977b6a318708d26dd6de02abf/releases/wiles-v#{version}.zip"
   name "Wiles"
   desc "Ultra-fast modern macOS File Manager"
   homepage "https://github.com/marcops/wiles"
